a2074e4930f32716b9aec0fcae17e14ce3c729b0
Lucas <e4worksoftwares@gmail.com>
2020-06-03 19:12:27 -0300

    Variaveis

---
+++ Variaveis de Memoria/exemplo01.php
@@ -0,0 +1,14 @@
+<?php 
+
+    /*
+    * Váriável é o espaçamento utilizado no servidor para armazenar dados.  
+    */
+
+    $nome = "Lucas";        // * Variável declarada.
+    
+    echo $nome;             // * Comando para exibição de dados na tela.
+
+    var_dump($nome);        // * Utilizada para verificar os dados da variável especificada.
+
+
+?>
